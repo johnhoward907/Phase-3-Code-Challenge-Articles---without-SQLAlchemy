91cfa98269624beec63308d3422eaed31beb93b0
John <jjhhoward1738@gmail.com>
2025-05-28 01:27:04 +0300

    [Feature]: Add database connection handler

--- lib/debug.py
+++ lib/debug.py
@@ -5,7 +5,8 @@ from lib.models.article import Article
 # Create entries
 author = Author.create("Howard")
 magazine = Magazine.create("AI Weekly", "Tech")
-article = Article.create("Understanding AGI", author.id, magazine.id)
+Article = Article.create("Understanding AGI", author.id, magazine.id)
+
 
 # Fetching
 print(author.articles())
